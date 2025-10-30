0a024c3b7836b72c26c81e22727ba011db157ca8
CD13ONX <drouvot.charlotte@gmail.com>
2025-10-30 09:28:13 +0100

    Ajout d'un fichier de log pour les alertes

--- monitoring_disque.sh
+++ monitoring_disque.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
+LOG_FILE="journal_monitoring_disk.log"
+
 echo "Espace disque disponible :"
 
 df -h | awk 'NR>1' | while read ligne; do # awk 'NR>1' saute l'entête
-  utilisation=$(echo "$ligne" | awk '{print $5}' | tr -d '%') #utilisation (%) : colonne 5
-  partition=$(echo "$ligne" | awk '{print $1}') #nom de la partition
+    utilisation=$(echo "$ligne" | awk '{print $5}' | tr -d '%') #utilisation (%) : colonne 5
+    partition=$(echo "$ligne" | awk '{print $1}') #nom de la partition
+    montage=$(echo "$ligne" | awk '{print $6}')
 
-  echo "$partition ($montage) → ${utilisation}% utilisé"
+    echo "$partition ($montage) → ${utilisation}% utilisé"
 
 # Si >80%
   if [ "$utilisation" -gt 80 ]; then
-    message="La partition a dépassé 80% d'utilisation"
-    echo $message
+    message="[ALERTE] $(date) : $partition ($montage) a dépassé le seuil de 80% et a atteint ${utilisation}% d’utilisation."
+    echo "$message" >> "$LOG_FILE"
   fi
 
 done
-
-
