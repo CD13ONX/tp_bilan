10cd761dbf0a6465719683e75a72026b74c648d2
CD13ONX <drouvot.charlotte@gmail.com>
2025-10-30 11:17:52 +0100

    Simulation affichage mail

--- backup/backup.py
+++ backup/backup.py
@@ -3,8 +3,14 @@ import datetime
 import os
 import sys
 
+
+def simulation_mail(destinataire, message):
+    mail = "----- Simulation envoi mail -----\n À : " + destinataire + "\n Message : " + message + " \n ---------------------------------"
+    return mail
+
 source = "backup/source_dir"
 destination = "backup/backup_dir"
+destinataire_email="admin@gmail.com"
 
 date = datetime.datetime.now().strftime("%Y-%m-%d-%Hh%M")
 backup_path = os.path.join(destination, f"backup_{date}") #chemin sauvegarde
@@ -20,8 +26,8 @@ cmd = f'xcopy "{source}" "{backup_path}" /E /I /Y'
 try:
     # execution commande + capture erreurs si existantes
     result = subprocess.run(cmd, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-    print("Sauvegarde ok")
+    print(simulation_mail(destinataire_email, "Sauvegarde ok"))
 
 except subprocess.CalledProcessError as e:
-    print("Erreur lors de la sauvegarde :")
-    print(e.stderr) #Affichage errreur
+    erreur=e.stderr
+    print(simulation_mail(destinataire_email, "Erreur lors de la sauvegarde : "+ erreur))
