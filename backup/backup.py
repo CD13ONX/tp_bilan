356e2dc3e0be203bfbde04cdb2bd29ae81a1032f
CD13ONX <drouvot.charlotte@gmail.com>
2025-10-30 11:05:24 +0100

    Gestion des erreurs backup

--- backup/backup.py
+++ backup/backup.py
@@ -5,12 +5,23 @@ import sys
 
 source = "backup/source_dir"
 destination = "backup/backup_dir"
-timestamp = datetime.datetime.now().strftime("%Y-%m-%d-%Hh%M")
-backup_path = os.path.join(destination, f"backup_{timestamp}")
-os.makedirs(backup_path, exist_ok=True)
 
+date = datetime.datetime.now().strftime("%Y-%m-%d-%Hh%M")
+backup_path = os.path.join(destination, f"backup_{date}") #chemin sauvegarde
+os.makedirs(backup_path, exist_ok=True) #création dossier sauvegarde
 
-# xcopy /E /I /Y : copie tout, crée dossier si nécessaire, pas de confirmation
+# Verification du dossier source
+if not os.path.exists(source):
+    print("Dossier source introuvable")
+
+ # xcopy /E /I /Y : copie tout, crée dossier si nécessaire, pas de confirmation
 cmd = f'xcopy "{source}" "{backup_path}" /E /I /Y'
-subprocess.run(cmd, shell=True)
 
+try:
+    # execution commande + capture erreurs si existantes
+    result = subprocess.run(cmd, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    print("Sauvegarde ok")
+
+except subprocess.CalledProcessError as e:
+    print("Erreur lors de la sauvegarde :")
+    print(e.stderr) #Affichage errreur
