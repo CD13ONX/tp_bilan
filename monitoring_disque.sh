31161d164857d1ad22f34058b59d236a88c8262a
CD13ONX <drouvot.charlotte@gmail.com>
2025-10-30 09:19:08 +0100

    Alerte si utilisation > 80%

--- monitoring_disque.sh
+++ monitoring_disque.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 
 echo "Espace disque disponible :"
-df -h
+
+df -h | awk 'NR>1' | while read ligne; do # awk 'NR>1' saute l'entête
+  utilisation=$(echo "$ligne" | awk '{print $5}' | tr -d '%') #utilisation (%) : colonne 5
+  partition=$(echo "$ligne" | awk '{print $1}') #nom de la partition
+
+  echo "$partition ($montage) → ${utilisation}% utilisé"
+
+# Si >80%
+  if [ "$utilisation" -gt 80 ]; then
+    message="La partition a dépassé 80% d'utilisation"
+    echo $message
+  fi
+
+done
+
+
